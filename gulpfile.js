cf51c964490916b811fc0ad5ba8e5ec3a68474c4
Danila Katyshev <idemvpered76@gmail.com>
2024-02-01 13:53:54 +0300

    gulp error 2

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,4 @@
-const {src, dest} = require('gulp');
+const {src, dest} = require('gulp'),
 
 gulp = require('gulp'), // подключаем Gulp через require
 include = require('gulp-include'), // сборка HTML (header, footer..)
@@ -8,7 +8,6 @@ sourceMaps = require('gulp-sourcemaps'), // нахождение строк в d
 uglify = require('gulp-uglify-es').default, // плагин для сжатия JS файлов
 concat = require('gulp-concat'), // плагин для переименования файлов
 cleancss = require('gulp-clean-css'), // убирает комментарии в стилях
-// autoprefixer = require('gulp-autoprefixer'),
 
 // IMAGES
 imageMin = require('gulp-imagemin'), // сжатие картинок + в новые форматы
@@ -20,7 +19,7 @@ webcss = require('gulp-webp-css'),
 fonter = require('gulp-fonter'),
 ttf2woff2 = require('gulp-ttf2woff2'),
 
-// browserSync = require('browser-sync').create(),
+browserSync = require('browser-sync').create(),
 clean = require('gulp-clean'), // возмож-сть удаления папки dist, работа с файл.сист
 fs = require('fs')
 
@@ -47,16 +46,6 @@ fs = require('fs')
 
 // import clean from 'gulp-clean'; // возмож-сть удаления папки dist, работа с файл.сист
 
-// function browserSync(){
-//     browserSync({
-//         server: {
-//           baseDir: "app/"
-//         },
-//         port: 8080,
-//         open: true,
-//         notify: false
-//       });
-// }
 
 function images(){
     return src('app/images/**/*')
@@ -78,6 +67,7 @@ function html(){
     }))
     .pipe(webhtml())
     .pipe(dest('dist'))
+    .pipe(browserSync.stream());
 }
 
 // Функции для компиляции стилей
@@ -90,9 +80,7 @@ function style(){
     .pipe(webcss())
     .pipe(sourceMaps.write('.'))
     .pipe(dest('dist/css')) 
-    // .pipe(browserSync.reload({
-    //     stream: true
-    //   }))
+    .pipe(browserSync.stream());
 }
 function style2(){
     return src('app/scss/pages/*.scss')  
@@ -102,9 +90,7 @@ function style2(){
     .pipe(webcss())
     .pipe(sourceMaps.write('.'))
     .pipe(dest('dist/css/pages-style')) 
-    // .pipe(browserSync.reload({
-    //     stream: true
-    //   }))
+    .pipe(browserSync.stream());
 }
 function style3(){
     return src('app/scss/components/*.scss')  
@@ -114,9 +100,7 @@ function style3(){
     .pipe(webcss())
     .pipe(sourceMaps.write('.'))
     .pipe(dest('dist/css/components')) 
-    // .pipe(browserSync.reload({
-    //     stream: true
-    //   }))
+    .pipe(browserSync.stream());
 }
 // Функции для компиляции стилей
 
@@ -126,6 +110,7 @@ function minjs(){
     .pipe(uglify())
     .pipe(concat('main.min.js'))
     .pipe(dest('dist/js'))
+    .pipe(browserSync.stream());
 }
 function extraJS(){
     return src('app/js/js-extra-settings/*.js')
@@ -140,15 +125,6 @@ function fonts(){
     .pipe(dest('dist/fonts'))
 }
 
-
-// function host(){
-//     return src('dist/pages/index.html')
-//     .pipe(server({
-//         livereload: true,
-//         open: true
-//     }))
-// }
-
 function cleaner(done){
     if (fs.existsSync('dist/')){  // Таск для удаления папки dist
         return src('dist',{read: false}).pipe(clean())
@@ -156,10 +132,18 @@ function cleaner(done){
     done()
 }
 
+
+
 function watching(){   
+    browserSync.init({
+        server: {
+            baseDir: "./dist"
+        }
+    });
+    gulp.watch(('dist/*.html', 'dist/components/*.html').on('change', browserSync.reload))
     gulp.watch(['app/*.html', 'app/components/*.html'], html)
     gulp.watch(['app/scss/**/*.scss'], gulp.series(style, style2, style3)) //,browserSync
-    gulp.watch(['app/img/**/*'], images)
+    gulp.watch(['app/imgages/**/*'], images)
     gulp.watch(['app/js/**/*.js'], minjs)
     gulp.watch(['app/fonts/*'], fonts)
 }
@@ -169,16 +153,13 @@ exports.style = style
 exports.style2 = style2
 exports.style3 = style3
 exports.minjs = minjs
-// exports.browserSync = browserSync
 exports.extraJS = extraJS
 exports.images = images
 exports.fonts = fonts
-// exports.host = host
 exports.cleaner = cleaner
 exports.watching = watching
 
 exports.default = gulp.series(
-    // browserSync,
     cleaner,
     gulp.parallel(html, style, style2, style3, minjs, extraJS, images, fonts), 
     gulp.parallel(watching)
